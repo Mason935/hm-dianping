3080cf73c0f28bbec87cdb06d7313f2c2a6b1315
gtao <1814793173@qq.com>
2023-05-12 14:45:48 +0800

    Redis全局唯一id

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,45 @@
+package com.hmdp.utils;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author gtao
+ * @date 2023/5/12 13:35
+ * @since 1.0.0
+ */
+@Component
+public class RedisIdWorker {
+
+    //开始时间戳
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+
+    //序列号的位数
+    private static final int COUNT_BITS = 32;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+
+    public long nextId(String keyPrefix) {
+
+        //1.生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+
+        //2.生成序列号
+        //2.1获取当前日期，精确到天
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        // 2.2.自增长（redis单个key的自增长有上限的，2^64）
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+        //拼接并返回
+        return timestamp << COUNT_BITS | count;
+    }
+}
